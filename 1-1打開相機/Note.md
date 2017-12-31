cae3623226518df899227a6ff1d293374a6a44ec
EZImageHCC <hc.chang@ezimage-tech.com>
2017-12-31 09:32:46 +0800

    1-1開啟相機 大致上完成 2017.12.31 HCChang

---
+++ 1-1打開相機/Note.md
@@ -0,0 +1,15 @@
+
+1-1 開啟相機
+
+-------------
+
+OpenCV 裡面是用 VideoCapture 這個物件可以直接創立
+
+如果到Source code裡面看 底層在Windows是用 DirectShow Linux則是用 V4l2
+
+OpenCV有個waitkey可以固定幾ms去偵測鍵盤的事件
+
+所以用此可以偵測是否輸入 o 如果是 則會離開程式
+
+
+
--- 1-1打開相機/OpenCamera.cpp
+++ 1-1打開相機/OpenCamera.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-int main(int argv, char** argc)
+int main(int argc, char** argv)
 {
+	cv::VideoCapture webCam(0);
+	if (!webCam.isOpened())
+		return -1;
 
+	char key = 'i';
+	cv::namedWindow("Image");
 
+	while (key != 'o')
+	{
+		cv::Mat imgFrame;
+		webCam >> imgFrame;
+
+		cv::imshow("Image", imgFrame);
+		
+		key = cv::waitKey(30);
+	}
 
 	return 0;
 }
